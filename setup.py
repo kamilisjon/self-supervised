8b33a73ee19ec9b8977bdfaba3f6318d8de9c940
kamilisjon <kamilis.jonk@gmail.com>
2025-05-17 15:20:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
-
-from pathlib import Path
 from setuptools import setup
-
-directory = Path(__file__).resolve().parent
-with open(directory / 'README.md', encoding='utf-8') as f:
-  long_description = f.read()
 
 setup(name='self-supervised',
       version='1.0.0',
-      author='Kamilis Jonkus',
-      license='MIT',
-      classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
-      ],
       install_requires=[
         "tinygrad==0.10.3",
         "opencv-python==4.11.0.86",
